a3b476840501a9835bea905649bd52f8fefea285
Lucia Avanu <avanulucia.miruna@yahoo.com>
2022-08-02 19:48:53 +0300

    double points final

--- src/components/double points/DoublePoints.js
+++ src/components/double points/DoublePoints.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import "./double-points.css";
 
-export const DoublePoints = () => {
+export const DoublePoints = (props) => {
+  const { onClick, userPoints, setPoints, setPage } = props;
   const [pokemon, setPokemon] = useState({});
 
   async function getData() {
@@ -16,15 +17,56 @@ export const DoublePoints = () => {
     getData();
   }, []);
 
-  console.log(pokemon);
+  function isEmpty(obj) {
+    for (var prop in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+        return false;
+      }
+    }
+    return JSON.stringify(obj) === JSON.stringify({});
+  }
 
-  return (
-    <div className="modal">
-      <div className="white-container">
-        <p className="title">Want a chance to double your points?</p>
-        <p>Guess the name of the pokemon down below</p>
-        <img src={pokemon.sprites.other["dream_world"]["front_default"]} />
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const checkGuess = () => {
+    if (userInput == pokemon.name) {
+      setPoints({ userPoints: userPoints * 2 });
+      setPage(2);
+    } else {
+      setPage(2);
+    }
+  };
+
+  if (!isEmpty(pokemon)) {
+    console.log(pokemon.name);
+    return (
+      <div className="modal">
+        <div className="white-container">
+          <img src={"./x.svg"} className="x" onClick={onClick} />
+          <p className="title">Want a chance to double your points?</p>
+          <p className="comment">Guess the name of the pokemon down below</p>
+          <img
+            className="hidden-pokemon"
+            src={pokemon.sprites.other["dream_world"]["front_default"]}
+          />
+          <div className="guess-container">
+            <input
+              type="text"
+              id="userInput"
+              name="userInput"
+              onChange={handleChange}
+              value={userInput}
+            />
+            <button className="guess-btn" onClick={checkGuess}>
+              Guess
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
