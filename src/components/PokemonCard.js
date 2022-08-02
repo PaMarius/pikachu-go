8a1ec0f3bfbb4633a1e60894598e67b27db7b7de
PaMarius <100645047+PaMarius@users.noreply.github.com>
2022-08-02 11:07:11 +0300

    Update PokemonCard.js

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,27 @@
 import "./pokemonCard.css";
 import { PokemonList } from "./PokemonList";
+import "./pokemonsTypeColor.css";
 
-export const PokemonCard = () => {
+export const PokemonCard = ({ pokemon }) => {
+  const style = `pokemon-img ${pokemon.types[0].type.name}`;
+  const styleForType = `type ${pokemon.types[0].type.name}`;
   return (
-  <div className='cards'>
-  <div className="pokemon-card"></div>
-  </div>
-  )
+    <div className="pokemon-card">
+      <div className="pokemon-appearance">
+        <div className={style}></div>
+        <img
+          className="img"
+          src={pokemon.sprites.other["dream_world"]["front_default"]}
+        />
+      </div>
+      <div className="pokemon-name">{pokemon.name.toUpperCase()}</div>
+      <div className="pokemon-types">
+        {pokemon.types.map((type) => {
+          return (
+            <div className={styleForType}>{type.type.name.toUpperCase()}</div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
