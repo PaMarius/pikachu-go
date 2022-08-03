0405e9a46bb6895e5a8e97b66259c1d66935f76e
Lucia Avanu <avanulucia.miruna@yahoo.com>
2022-08-03 13:22:20 +0300

    rules and guidelines page

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { RulesAndGuidelines } from "./rules and guidelines/RulesAndGuidelines";
 import "./startPage.css";
 
 function StartPage({ setPage }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const burgerHandler = () => {
+    setModalIsOpen(true);
+  };
+  const closeModalHandler = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className="container">
-      <button>Click me</button>
+      <button onClick={burgerHandler}>Click me</button>
+      {modalIsOpen && <RulesAndGuidelines onClick={closeModalHandler} />}
       <button onClick={() => setPage(1)} className="start-button">
         START GAME
       </button>
