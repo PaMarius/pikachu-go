c269ef6752df86a5d3508b8818bd42bfe130202f
Lucia Avanu <avanulucia.miruna@yahoo.com>
2022-08-03 12:13:51 +0300

    backup

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -4,6 +4,7 @@ import "./startPage.css";
 function StartPage({ setPage }) {
   return (
     <div className="container">
+      <button>Click me</button>
       <button onClick={() => setPage(1)} className="start-button">
         START GAME
       </button>
---
+++ src/components/rules and guidelines/RulesAndGuidelines.js
@@ -0,0 +1,11 @@
+import "./RulesAndGuidelines.css";
+
+export const RulesAndGuidelines = () => {
+  return (
+    <div className="modal">
+      <div className="white-container">
+        <p className="title">Rules and Guidelines</p>
+      </div>
+    </div>
+  );
+};
